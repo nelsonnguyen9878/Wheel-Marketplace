42f210027ef266eba9aa1dacfe289d37521749e5
Nelson Nguyen <nelsonnguyen@Nelsons-MacBook-Pro.local>
2025-05-17 15:55:38 -0700

    Started implementing socket.io into server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,20 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const cors = require('cors')
-const http = require("http")
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require('cors');
+const http = require("http");
+const { Server } = require("socket.io");
 
 // 
-const app = express()
+const app = express();
+
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
 
 app.listen(4000, () => {
     console.log('listening on port 4000')
